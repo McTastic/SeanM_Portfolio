6b6d07c23e320a27289830316f5111b3fcc33862
Sean M <mctastic2121@gmail.com>
2023-01-20 00:20:28 -0500

    updated projects page

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -5,29 +5,63 @@ import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import { CardActionArea, Grid } from "@mui/material";
 import { projects } from "../utils/projectData";
+import GitHubIcon from "@mui/icons-material/GitHub";
 
 export default function ActionAreaCard() {
   return (
     <>
       {projects.map((project) => (
-        <Grid item xs={12} sm={6} md={4} lg={6} xl={6} key={project.name} display="flex" justifyContent={"center"}>
-          <Card sx={{ maxWidth: 800, marginTop: 15, boxShadow: 10 }}>
+        <Grid
+          item
+          xs={12}
+          sm={6}
+          md={4}
+          lg={6}
+          xl={6}
+          key={project.name}
+          display="flex"
+          justifyContent={"center"}
+        >
+          <Card
+            sx={{
+              maxWidth: 800,
+              marginTop: 15,
+              marginBottom: 2,
+              boxShadow: "15px 15px  rgb(33,150,115) , -15px -15px rgb(33,150,55)",
+              borderRadius:"10px"
+            }}
+          >
             <CardActionArea>
               <CardMedia
                 component="img"
-                height="450"
-                image= {project.picture}
+                height="350"
+                image={project.picture}
                 alt="project picture"
-                onClick={(e)=> window.open(project.url)}
+                onClick={(e) => window.open(project.url)}
               />
               <CardContent>
                 <Typography gutterBottom variant="h4" component="h4">
                   {project.name}
                 </Typography>
-                <Typography variant="body2" color="text.secondary" fontSize="1.5em">
+                <Typography
+                  variant="body2"
+                  color="text.secondary"
+                  fontSize="1.5em"
+                >
                   {project.description}
                 </Typography>
               </CardContent>
+              <GitHubIcon
+                fontSize="large"
+                onClick={(e) => window.open(project.github)}
+                sx={{
+                  transition: "all .5s",
+                  "&:hover":{
+                    transform: "scale(1.45)",
+                    color: "rgb(1,99,155)"
+                  }
+                }}
+              />
             </CardActionArea>
           </Card>
         </Grid>
--- src/utils/projectData.js
+++ src/utils/projectData.js
@@ -1,9 +1,7 @@
-import image1 from "../images/xmasScreenshot.png";
+import image1 from "../images/chatapp.png";
 import image2 from "../images/screenshotTGuide.png";
 import image3 from "../images/ptc-binder-pic.jpg";
 import image4 from "../images/weatherScreenshot.png";
-import image5 from "../images/budgetScreenshot.png";
-import image6 from "../images/workoutScreenshot.png";
 
 export const projects = [
   {
@@ -12,13 +10,15 @@ export const projects = [
       "A full stack web application that utilizes Next.js for page distrobution. This app allows the user to have a digital tracker for their Pokemon Card collection",
     picture: image3,
     url: "https://ptc-binder-v2-team-mc.vercel.app/",
+    github: "https://github.com/McTastic/PTC-Binder_V2"
   },
   {
-    name: "Christmas Movie Workshop",
+    name: "Super Chill Chat",
     description:
-      "Full-Stack website utilizing the MERN stack. Allows the user to rate movies on a 1 to 5 scale utilizing the imDB API",
+      "Fun little chat app that allows users to communicate with each other via multiple chat rooms. Utilizes web sockets via socket-io. Utilizes a basic express server and Remix.js",
     picture: image1,
-    url: "https://xmas-movie-db.herokuapp.com/",
+    url: "https://chill-chat.onrender.com",
+    github: "https://github.com/McTastic/Chat_App"
   },
   {
     name: "Travel Guide",
@@ -26,6 +26,7 @@ export const projects = [
       "Front end web application utilizing the open weather and wikipedia APIs to find information on specified country/city",
     picture: image2,
     url: "https://jkohrt7.github.io/Digital_Travel_Guide/",
+    github: "https://github.com/jkohrt7/Digital_Travel_Guide"
   },
   {
     name: "Weather Dashboard",
@@ -33,19 +34,6 @@ export const projects = [
       "This is a basic front-end application utilizing the OpenWeather API. This allows users to search for a city and displays the current weather",
     picture: image4,
     url: "https://mctastic.github.io/Weather-Dashboard/",
-  },
-  {
-    name: "Budget Tracker",
-    description:
-      "Full Stack web app that utilizes MongoDB for it's back-end data. Allows the user to submit amounts (in dollars) to track their budget and total balance",
-    picture: image5,
-    url: "https://budget-tracker3232.herokuapp.com/",
-  },
-  {
-    name: "Workout Tracker",
-    description:
-      "Full Stack web app that utlizes MongoDB for it's back-end data. Allows the user to track workouts that they have completed or are in the process of completing. ",
-    picture: image6,
-    url: "https://damp-oasis-83805.herokuapp.com/?id=61bf7ff6c7cf390016af8fde",
+    github: "https://github.com/McTastic/Weather-Dashboard"
   },
 ];
